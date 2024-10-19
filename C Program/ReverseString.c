686b6011e688a8552925222fc9e72b8ec08a5c7f
Elamathi Selvan <elamathis8060@gmail.com>
2024-10-19 09:46:22 +0530

    Update ReverseString.c

--- C Program/ReverseString.c
+++ C Program/ReverseString.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
 #include <string.h>
-int main()
-{
-   char s[100];
 
-   printf("Enter a string to reverse\n");
-   gets(s);
+void reverse_string(char *str) {
+    int n = strlen(str);
+    for (int i = 0; i < n / 2; i++) {
+        // Swap characters
+        char temp = str[i];
+        str[i] = str[n - i - 1];
+        str[n - i - 1] = temp;
+    }
+}
 
-   strrev(s);
+int main() {
+    char s[100];
 
-   printf("Reverse of the string: %s\n", s);
+    printf("Enter a string to reverse:\n");
+    fgets(s, sizeof(s), stdin); // Use fgets instead of gets
 
-   return 0;
+    // Remove the newline character if present
+    size_t len = strlen(s);
+    if (len > 0 && s[len - 1] == '\n') {
+        s[len - 1] = '\0';
+    }
+
+    reverse_string(s); // Call the function to reverse the string
+
+    printf("Reverse of the string: %s\n", s);
+
+    return 0;
 }
